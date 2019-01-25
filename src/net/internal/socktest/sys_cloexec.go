b33fe22ed7d493d6a952a1bf26fd754ddc6f72e0
Calvin Hill <calvin@hakobaito.co.uk>
2019-01-25 21:52:33 +0000

    net: Port the net package to Haiku

--- src/net/internal/socktest/sys_cloexec.go
+++ src/net/internal/socktest/sys_cloexec.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build dragonfly || freebsd || illumos || linux || netbsd || openbsd
+//go:build dragonfly || freebsd || haiku || illumos || linux || netbsd || openbsd
 
 package socktest
 
