49566cbc74349430a968d1eab9df4b981c04a793
Jérôme Duval <jerome.duval@gmail.com>
2022-06-02 11:28:19 +0200

    os: add dirent for Haiku
    enable user common code for Haiku

--- src/os/user/listgroups_unix.go
+++ src/os/user/listgroups_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build ((darwin || dragonfly || freebsd || (js && wasm) || (!android && linux) || netbsd || openbsd || solaris) && (!cgo || osusergo)) || aix || illumos
+//go:build ((darwin || dragonfly || freebsd || haiku || (js && wasm) || (!android && linux) || netbsd || openbsd || solaris) && (!cgo || osusergo)) || aix || illumos
 
 package user
 
